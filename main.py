46bbe5d27f97515ea8adeb859476b368aa1cc6b3
aph <aphiscool1@gmail.com>
2025-03-09 15:46:38 -0700

    fix

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ subreddits = [
 
 def createVideo(thread):
     print(Fore.GREEN + "Fetching Data From Reddit Thread")
-    title, replies = getRedditData("https://www.reddit.com/r/AskReddit/comments/1j58taz/people_who_knew_a_killer_did_you_ever_suspect/")
+    title, replies = getRedditData(thread)
     print(Fore.GREEN + "Generating Initial Title Screenshot")
     getScreenshot(title)
     wordLowerBound = 200
-    wordUpperBound = 350
+    wordUpperBound = 300
     filteredReplies = []
     for reply in replies:
         upvotes, words, text = reply
@@ -30,6 +30,7 @@ def createVideo(thread):
     # TEMP
     # TEMP
     # filteredReplies = filteredReplies[0]
+    print(Fore.RED+f"Found {len(filteredReplies)} Suitable Comments")
     for filteredReply in filteredReplies:
         print(Fore.GREEN + "Compiling Video")
         compileVideo(title,filteredReply)
@@ -48,11 +49,11 @@ def createVideo(thread):
 
 
 threads = [
-    "https://www.reddit.com/r/AskReddit/comments/1j58taz/people_who_knew_a_killer_did_you_ever_suspect/",
+    # "https://www.reddit.com/r/AskReddit/comments/1j58taz/people_who_knew_a_killer_did_you_ever_suspect/",
     # "https://www.reddit.com/r/AskReddit/comments/1j29dcb/serious_people_who_grew_up_in_a_small_town_what/",
     # "https://www.reddit.com/r/AskReddit/comments/1j1e24y/whats_a_scam_so_good_that_people_dont_even/",
     # "https://www.reddit.com/r/AskReddit/comments/1j5ejd2/those_of_you_who_grew_up_poor_what_was_your/",
-    # "https://www.reddit.com/r/AskReddit/comments/1j3bba5/whats_the_most_terrifying_we_need_to_leave_now/",
+    "https://www.reddit.com/r/AskReddit/comments/1j3bba5/whats_the_most_terrifying_we_need_to_leave_now/",
     # "https://www.reddit.com/r/AskReddit/comments/1j2llnm/whats_a_universally_loved_food_that_you_secretly/",
     # "https://www.reddit.com/r/AskReddit/comments/1j4o19p/people_who_unsubscribed_from_their_once_favorite/",
     # "https://www.reddit.com/r/AskReddit/comments/1j20m7s/what_is_the_disturbing_backstory_behind_something/",

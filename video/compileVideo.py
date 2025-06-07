b6bbefde442157fdbce39941952b0889cde29e8e
aph <aphiscool1@gmail.com>
2025-06-07 15:19:31 -0700

    remove extra speed

--- video/compileVideo.py
+++ video/compileVideo.py
@@ -13,33 +13,49 @@ from video.getTTS import getTTS
 fileDir = os.path.dirname(os.path.abspath(__file__))
 
 
-def adjust_subtitle_timings(subtitles, original_duration):
+def adjust_subtitle_timings(subtitles, original_duration, new_duration):
     """Adjust subtitle timings based on speed-up factor."""
-    speed_factor = original_duration / 60  # e.g., 120 / 60 = 2
+    speed_factor = original_duration / new_duration  # e.g., 120 / 60 = 2
     adjusted_subtitles = []
 
     for subtitle in subtitles:
         new_start = subtitle.start / speed_factor
-        new_duration = subtitle.duration / speed_factor
-        adjusted_subtitle = subtitle.with_start(new_start).with_duration(new_duration)
+        new_end = subtitle.duration / speed_factor
+        adjusted_subtitle = subtitle.with_start(new_start).with_duration(new_end)
         adjusted_subtitles.append(adjusted_subtitle)
 
     return adjusted_subtitles
 
 def compileVideo(title,bestReply):
-    print(Fore.GREEN + "Obtaining Background Video")
-    bg_video = getBackgroundVideo()
-    print(Fore.GREEN + "Adding Initial Title Screenshot")
-    image = ImageClip(os.path.join(fileDir, "../screenshot/reddit_screenshot.png")).with_duration(bg_video.duration)
-    image = image.with_duration(4).with_position("center")  # Adjust position if needed
 
-    print(Fore.GREEN + "Generating TTS")
+
+    print(Fore.GREEN + "[+] Generating TTS")
+    print(Fore.RESET)
     getTTS(title,bestReply)
     main_audio = AudioFileClip(os.path.join(fileDir, "./tts.mp3"))
     original_duration = main_audio.duration
-    main_audio = MultiplySpeed(final_duration=60).apply(main_audio)
+    new_duration = original_duration * 1
+    main_audio = MultiplySpeed(final_duration=new_duration).apply(main_audio)
 
-    print(Fore.GREEN + "Adding Background Music")
+
+
+    print(Fore.GREEN + "[+] Obtaining Background Video")
+    print(Fore.RESET)
+    bg_video = getBackgroundVideo(duration=new_duration)
+    print(Fore.GREEN + "[+] Adding Initial Title Screenshot")
+    print(Fore.RESET)
+    image = ImageClip(os.path.join(fileDir, "../screenshot/reddit_screenshot.png")).with_duration(bg_video.duration)
+    image = image.with_duration(4).with_position("center")  # Adjust position if needed
+
+
+
+
+
+
+
+
+    print(Fore.GREEN + "[+] Adding Background Music")
+    print(Fore.RESET)
     bg_audio_dir = os.path.join(fileDir, "./background_audio")
     bg_audio_file = random.choice([f for f in os.listdir(bg_audio_dir) if f.endswith(".mp3")])
     bg_audio_path = os.path.join(bg_audio_dir, bg_audio_file)
@@ -48,12 +64,14 @@ def compileVideo(title,bestReply):
 
 
     final_audio = CompositeAudioClip([main_audio, bg_audio.with_start(0)])
-    print(Fore.GREEN + "Adding Subtitles")
+    print(Fore.GREEN + "[+] Adding Subtitles")
+    print(Fore.RESET)
     subtitles = getSubtitles()
 
-    adjusted_subtitles = adjust_subtitle_timings(subtitles, original_duration)
+    adjusted_subtitles = adjust_subtitle_timings(subtitles, original_duration, new_duration)
 
-    print(Fore.GREEN + "Assembling Video")
+    print(Fore.GREEN + "[+] Assembling Video")
+    print(Fore.RESET)
     final_video = CompositeVideoClip([bg_video] + adjusted_subtitles + [image])
 
     final_video = final_video.with_audio(final_audio)
@@ -61,6 +79,7 @@ def compileVideo(title,bestReply):
     random_uuid = uuid.uuid4()
     fileName = f"{str(random_uuid)}.mp4"
     print(Fore.RED + f"Saving Video As {fileName}")
+    print(Fore.RESET)
     final_video.write_videofile(os.path.join(fileDir, f"../output/{fileName}"), codec="libx264", fps=30,logger='bar')
 
     return
